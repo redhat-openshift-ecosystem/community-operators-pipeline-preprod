cbc6a781b7c29d5294d555fab9c0cf026956f228
Martin Jediny <jedinym@gmail.com>
2023-08-28 13:57:55 +0200

    [ISV-3897] Add dummy github API call for testing

--- ci/prow/main.sh
+++ ci/prow/main.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 echo "Prow script initiated."
 oc whoami
+
+HTTP_RESPONSE=$(curl -L -I \
+    -u operator-bundle-bot:$(/var/run/cred/operator_bundle_bot_github_token) \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/emojis)
+
+HTTP_STATUS=$(echo ${HTTP_RESPONSE} | head --lines 1 | cut -d ' ' -f 2)
+
+if [ ${HTTP_STATUS} = 200 ]; then
+    echo "Operator bot available."
+else
+    echo ${HTTP_RESPONSE}
+    >&2 echo "Operator bot NOT available!"
+fi
+
 echo "Prow complete."
