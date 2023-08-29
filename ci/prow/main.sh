2fa528bcccf85768933a1d90124a03b7095422e8
Martin Jediny <jedinym@gmail.com>
2023-08-29 12:45:38 +0200

    Fix secret check

--- ci/prow/main.sh
+++ ci/prow/main.sh
@@ -2,19 +2,17 @@
 echo "Prow script initiated."
 oc whoami
 
-HTTP_RESPONSE=$(curl -L -I \
-    -u operator-bundle-bot:$(/var/run/cred/operator_bundle_bot_github_token) \
-    -H "Accept: application/vnd.github+json" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/emojis)
-
-HTTP_STATUS=$(echo ${HTTP_RESPONSE} | head --lines 1 | cut -d ' ' -f 2)
-
-if [ ${HTTP_STATUS} = 200 ]; then
-    echo "Operator bot available."
+if [ -f /var/run/cred/operator_bundle_bot_github_token ]; then
+  echo "Operator bot github token available."
 else
-    echo ${HTTP_RESPONSE}
-    >&2 echo "Operator bot NOT available!"
+  >&2 echo "Operator bot github token NOT available!"
+  exit 1
 fi
+
+# HTTP_RESPONSE=$(curl -L -I \
+#     -u operator-bundle-bot:$(/var/run/cred/operator_bundle_bot_github_token) \
+#     -H "Accept: application/vnd.github+json" \
+#     -H "X-GitHub-Api-Version: 2022-11-28" \
+#     https://api.github.com/emojis)
 
 echo "Prow complete."
