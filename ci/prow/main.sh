f349471de5dda045475141ccba79edaef08a7161
Maurizio Porrato <mporrato@redhat.com>
2023-08-30 13:21:33 +0100

    Test github access from prow job

--- ci/prow/main.sh
+++ ci/prow/main.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "Prow script initiated."
 oc whoami
+read GITHUB_TOKEN </var/run/cred/operator_bundle_bot_github_token
+export GITHUB_TOKEN
+PR_URL=$(echo "${CLONEREFS_OPTIONS}" | jq -r '.refs[0].pulls[0].link')
+gh pr view "${PR_URL}" --json title,state -q '"[\(.state)] \(.title)"'
 echo "Prow complete."
