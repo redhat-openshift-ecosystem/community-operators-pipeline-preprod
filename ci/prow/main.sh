839fb621ffa34f2b04e5dd1af31ec591c2c1eb41
tbak <tbak@redhat.com>
2023-09-11 21:38:44 +0200

    [ISV-3982] create entrypoint for prow

--- ci/prow/main.sh
+++ ci/prow/main.sh
@@ -61,7 +61,7 @@ gh pr edit "${pr_url}" \
 
 echo "Starting test at $(date)"
 
-sleep 15    # Actual test should be triggered here
+python3 operatorcert/entrypoints/start_community_prow.py
 
 gh pr edit "${pr_url}" \
     --remove-label "ocp/${OCP_CLUSTER_VERSION}/running" \
